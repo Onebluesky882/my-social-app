6457400989d1ff40eaf3c3db69184ea86eda29a8
Onebluesky882 <onebluesky882@outlook.com>
2025-04-07 00:34:27 +0700

    deploy first time

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/app/components/theme-provider";
 import Header from "@/app/components/ui/header";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({
   children,
