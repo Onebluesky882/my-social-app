037f42be215ed8ce35703247eba444824020ef98
Onebluesky882 <onebluesky882@outlook.com>
2025-04-11 14:23:44 +0700

    menuHamburger

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -5,30 +5,27 @@ import { useEffect, useRef, useState } from "react";
 const MobileMenu = () => {
   const [isMobile, setIsMobile] = useState(false);
   const menuRef = useRef<HTMLDivElement | null>(null);
+
   const menuHamburger = () => {
     setIsMobile((prev) => !prev);
   };
 
   useEffect(() => {
     const handleOutside = (event: MouseEvent) => {
-      if (
-        menuRef.current &&
-        !menuRef.current.contains(event.target as unknown as Node)
-      ) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
         setIsMobile(false);
       }
     };
 
     if (isMobile) {
       document.addEventListener("mousedown", handleOutside);
-    } else {
-      document.removeEventListener("mousedown", handleOutside);
     }
 
     return () => {
       document.removeEventListener("mousedown", handleOutside);
     };
-  });
+  }, [isMobile]);
+
   return (
     <div>
       <div
@@ -42,9 +39,15 @@ const MobileMenu = () => {
       {isMobile && (
         <div
           ref={menuRef}
-          className="flex flex-col items-center justify-center gap-8 font-medium relative left-0 top-24 w-full h[calc(100vh-96px)] bg-card outline-1"
+          className="flex flex-col items-center justify-center gap-8 font-medium absolute left-0 top-24 w-full h[calc(100vh-96px)] bg-card z-10"
         >
-          <div>Home</div>
+          <div
+            onClick={() => {
+              console.log("hello");
+            }}
+          >
+            Home
+          </div>
           <div>Friend</div>
           <div>Stories</div>
           <div>Login</div>
