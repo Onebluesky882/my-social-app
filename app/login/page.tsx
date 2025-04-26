c292a784956b656403f186081f9ccbbf2ea5e790
Onebluesky882 <onebluesky882@outlook.com>
2025-04-26 20:36:54 +0700

    create : login form

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginWithGoogle from "@/components/LoginWithGoogle";
+
+export default async function SignInWithGoogle() {
+  return (
+    <div>
+      <LoginWithGoogle />
+    </div>
+  );
+}
